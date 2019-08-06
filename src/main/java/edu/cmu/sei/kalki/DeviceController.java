0a34b95bf9dfe49710e6494bb017096405872c95
cam292 <camazzotta@sei.cmu.edu>
2019-08-06 15:40:52 -0400

    Moved Rulebook package from kalki-iot-interface to here

--- src/main/java/edu/cmu/sei/kalki/DeviceController.java
+++ src/main/java/edu/cmu/sei/kalki/DeviceController.java
@@ -1,4 +1,8 @@
-
+// Rulebook imports, for later reference
+//import com.deliveredtechnologies.rulebook.Fact;
+//import com.deliveredtechnologies.rulebook.FactMap;
+//import com.deliveredtechnologies.rulebook.NameValueReferableMap;
+//import com.deliveredtechnologies.rulebook.model.runner.RuleBookRunner;
 public class DeviceController {
     public static void main(String[] args) {
         System.out.println("gradle configured");
