7590843801462283c85fe7f0dd6d41a84d72a6cc
cam292 <camazzotta@sei.cmu.edu>
2019-08-19 14:37:20 -0400

    Check for devices added prior to startup

--- src/main/java/edu/cmu/sei/kalki/database/DatabaseListener.java
+++ src/main/java/edu/cmu/sei/kalki/database/DatabaseListener.java
@@ -1,8 +1,11 @@
 package edu.cmu.sei.kalki.database;
 
+import edu.cmu.sei.ttg.kalki.database.Postgres;
 import edu.cmu.sei.ttg.kalki.listeners.InsertHandler;
 import edu.cmu.sei.ttg.kalki.listeners.InsertListener;
 import edu.cmu.sei.ttg.kalki.models.Device;
+
+import java.util.List;
 import java.util.logging.Logger;
 
 public class DatabaseListener {
@@ -16,5 +19,12 @@ public class DatabaseListener {
         InsertListener.addHandler("deviceupdate", new DeviceHandler("/update-device"));
         InsertListener.addHandler("devicesecuritystateinsert", new NewSecurityStateHandler());
         logger.info("[DatabaseListener] Initialized 3 database listeners.");
+
+        // get devices already inserted in system
+        List<Device> deviceList = Postgres.findAllDevices();
+        DeviceHandler tempHandler = new DeviceHandler("/new-device");
+        for (Device d: deviceList){
+            tempHandler.handleNewInsertion(d.getId());
+        }
     }
 }
