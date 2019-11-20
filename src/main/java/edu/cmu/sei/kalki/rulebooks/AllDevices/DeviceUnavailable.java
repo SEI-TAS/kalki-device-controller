8aa9708cd1f8a7cea7660c50bc43d6797e8df970
cam292 <camazzotta@sei.cmu.edu>
2019-11-20 14:11:25 -0500

    Updated device unavailable rule

--- src/main/java/edu/cmu/sei/kalki/rulebooks/AllDevices/DeviceUnavailable.java
+++ src/main/java/edu/cmu/sei/kalki/rulebooks/AllDevices/DeviceUnavailable.java
@@ -14,19 +14,25 @@ public class DeviceUnavailable extends RulebookRule {
     public boolean conditionIsTrue() {
         setAlertCondition("device-unavailable");
 
-        List<DeviceStatus> deviceStatuses = Postgres.findSubsetNDeviceStatuses(device.getId(), 1, status.getId());
+        //get last 5 statuses
+        List<DeviceStatus> deviceStatuses = Postgres.findSubsetNDeviceStatuses(device.getId(), 5, status.getId());
 
-        // if diff between timestamps is > 5*sampling rate
-        if(deviceStatuses.size() > 0){
-            long timestamp2 = status.getTimestamp().getTime();
-            long timestamp1 = deviceStatuses.get(0).getTimestamp().getTime();
-            if(Math.abs(timestamp2 - timestamp1) > 5*device.getSamplingRate()) {
-                alertInfo = "Timestamp difference: " + (Math.abs(timestamp2 - timestamp1));
-                return true;
-            }
-
+        //check that all 5 are null
+        for(DeviceStatus status: deviceStatuses){
+            if(status.getAttributes() != null) //if one is not null, device is available
+                return false;
         }
+        // if diff between timestamps is > 5*sampling rate
+//        if(deviceStatuses.size() > 0){
+//            long timestamp2 = status.getTimestamp().getTime();
+//            long timestamp1 = deviceStatuses.get(0).getTimestamp().getTime();
+//            if(Math.abs(timestamp2 - timestamp1) > 5*device.getSamplingRate()) {
+//                alertInfo = "Timestamp difference: " + (Math.abs(timestamp2 - timestamp1));
+//                return true;
+//            }
+//
+//        }
 
-        return false;
+        return true;
     }
 }
