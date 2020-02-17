eb9e597edb8a73e2974eef4fa277798a89fb5d16
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-02-17 16:40:45 -0500

    Isolated IoTI API code to new class, added config file for DB and DataNode configs

--- src/main/java/edu/cmu/sei/kalki/database/DeviceHandler.java
+++ src/main/java/edu/cmu/sei/kalki/database/DeviceHandler.java
@@ -1,72 +1,40 @@
 package edu.cmu.sei.kalki.database;
 
+import edu.cmu.sei.kalki.IoTInterfaceAPI;
 import edu.cmu.sei.ttg.kalki.database.Postgres;
 import edu.cmu.sei.ttg.kalki.listeners.InsertHandler;
 import edu.cmu.sei.ttg.kalki.models.Device;
 import edu.cmu.sei.ttg.kalki.models.StageLog;
-import org.json.JSONObject;
 
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.logging.Logger;
 
 public class DeviceHandler implements InsertHandler {
     private Logger logger = Logger.getLogger("device-controller");
-    private final int ATTEMPTS = 15;
+    private IoTInterfaceAPI iotInterface = new IoTInterfaceAPI();
 
-    private String apiUrl;
-    private boolean newDevice;
+    private boolean isNewDevice;
 
-    DeviceHandler(String endpoint, boolean newDev) {
-        apiUrl = endpoint;
-        newDevice = newDev;
+    DeviceHandler(boolean isNewDev) {
+        isNewDevice = isNewDev;
     }
 
     @Override
     public void handleNewInsertion(int newDeviceId) {
-        logger.info("[DeviceHandler] Device found: "+newDeviceId+". Is it new? "+newDevice);
+        logger.info("[DeviceHandler] Device found: "+newDeviceId+". Is it new? "+ isNewDevice);
         Device device = Postgres.findDevice(newDeviceId);
         logSampleRateIncreaseReact(device);
-        sendToIotInterface(device);
-    }
-
-    private void sendToIotInterface(Device dev) {
-        for(int i=0; i<ATTEMPTS; i++){
-            try {
-                URL url = new URL(this.apiUrl);
-                HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
-                httpCon.setDoOutput(true);
-                httpCon.setRequestMethod("POST");
-                OutputStreamWriter out = new OutputStreamWriter(httpCon.getOutputStream());
-                JSONObject json = new JSONObject(dev.toString());
-                out.write(json.toString());
-                out.close();
-                httpCon.getInputStream();
-                break;
-            } catch (Exception e) {
-                logger.severe("[DeviceHandler] Error sending device to IoT Interface API "+apiUrl+": "+dev.toString());
-                logger.severe(e.getMessage());
-                logger.severe("[DeviceHandler] Attempting to reconnect...");
-                sleep(1000);
-            }
+        if(isNewDevice) {
+            iotInterface.sendNewDeviceInfo(device);
+        } else {
+            iotInterface.sendUpdatedDeviceInfo(device);
         }
-
     }
 
     private void logSampleRateIncreaseReact(Device device) {
-        if(!newDevice){
+        if(!isNewDevice){
             StageLog log = new StageLog(device.getCurrentState().getId(), StageLog.Action.INCREASE_SAMPLE_RATE, StageLog.Stage.REACT, "Device updated: "+device.getId());
             log.insert();
             logger.info("[DeviceHandler] Logging react to device being updated: "+log.toString());
-        }
-    }
-
-    private void sleep(int millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (Exception e) {
-            logger.severe("[DeviceHandler] Error attempting to sleep: "+e.getMessage());
         }
     }
 }
